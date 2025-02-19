aad00cf38087b45dc60f530cf966b405c990d3fb
Raul Franco <43234547+raulfb@users.noreply.github.com>
2025-02-19 12:56:35 +0100

    Consume la ruta /protegida desde el componente Success

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 
-export const useStore = defineStore('storeId', {
+export const useStore = defineStore('main', {
 	state: () => {
 		return {
 			token:'',
--- frontend/src/views/pages/Success.vue
+++ frontend/src/views/pages/Success.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center justify-center">
-        <h1>Successful</h1>
+        <p>{{ mensaje }}</p>
     </div>
     <div class="flex flex-col items-center justify-center">
         <Button @click="volver">volver</Button>
@@ -8,13 +8,48 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { useStore } from "../../../src/stores/store.js";
+import axios from "axios";
 
 const router = useRouter();
+const store = useStore();
+const url = import.meta.env.VITE_APP_API_URL;
+let mensaje = ref("");
 
 function volver(){
     router.push({ path: "/" });
 }
 
+function getRutaProtegida() {
+  const goRutaProtegida = async () => {
+    let config = {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        "Authorization": store.token,
+      },
+    };
 
+    try {
+      return await axios.get(
+        url + "protegida",
+        config
+      );
+    } catch (error) {
+        mensaje.value = "Error al obtener la ruta protegida";
+    }
+  };
+
+  const processRutaProtegida = async () => {
+    const get_result = await goRutaProtegida();
+    if (get_result) {
+        mensaje.value = get_result.data.message;
+    }
+  };
+  processRutaProtegida();
+}
+
+getRutaProtegida();
 </script>
